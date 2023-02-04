7a7119ac0ddd5ae49749ba61cf4eccd448c8d57d
AnyMoonS <72291751+AnyMoonS@users.noreply.github.com>
2023-02-05 00:08:35 +0800

    Update AutoFinishLessons.py
    添加图形化交互界面

--- AutoFinishLessons.py
+++ AutoFinishLessons.py
@@ -3,22 +3,51 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 import threading
+from tkinter import *
+import tkinter as tk
 
-import config
+root = Tk()  # 创建主窗口
+root.title("AutoFinishLessons    By AnyMoonS")
+
+
+def center_window(root, width, height):  # 窗口居中函数
+    screenwidth = root.winfo_screenwidth()  # 获取显示屏宽度
+    screenheight = root.winfo_screenheight()  # 获取显示屏高度
+    size = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)  # 设置窗口居中参数
+    root.geometry(size)  # 让窗口居中显示
+
+
+center_window(root, 360, 240)
+root.resizable(0, 0)
+
+Label(root, text='智慧树账号（手机号）：').grid(row=0, column=0, padx=10, pady=30)
+Label(root, text='智慧树密码：').grid(row=1, column=0, padx=0, pady=0)
+
+userid = tk.StringVar()  # 创建账号与密码变量
+password = tk.StringVar()
+
+entryuserid = tk.Entry(root, textvariable=userid).grid(row=0, column=1, padx=0, pady=30)
+entrypassword = tk.Entry(root, textvariable=password, show='*').grid(row=1, column=1, padx=0, pady=10)
+
+Button(root, text='登录', width=10, command=root.destroy).grid(row=2, column=1, sticky=W, padx=0, pady=30)  # 登录
+
+root.mainloop()  # 主窗口进入循环
 
 driver = webdriver.Firefox()
 actions = ActionChains(driver)
 driver.get("https://onlineweb.zhihuishu.com/")  # 打开智慧树官网
 driver.maximize_window()
-time.sleep(5)
+time.sleep(2)
 
 
 def login():
+    luserid = userid.get()
+    lpassword = password.get()
     phonenumber = driver.find_element(By.ID, "lUsername")
     pw = driver.find_element(By.ID, "lPassword")
     loginbutton = driver.find_element(By.CLASS_NAME, "wall-sub-btn")
-    phonenumber.send_keys(config.userid)
-    pw.send_keys(config.password)
+    phonenumber.send_keys(luserid)
+    pw.send_keys(lpassword)
     loginbutton.click()
 
 
@@ -27,7 +56,7 @@ def lesson():
         if driver.current_url.__contains__("studyvideoh5"):  # 检测打开视频播放页
             time.sleep(10)  # 等待手动关闭弹窗
             video = driver.find_element(By.CLASS_NAME, "videoArea")  # 定位视频窗口
-            video.click()#播放视频
+            video.click()  # 播放视频
             try:
                 ActionChains(driver).move_to_element(video).perform()
                 speedbox = driver.find_element(By.CLASS_NAME, "speedBox")
@@ -66,9 +95,10 @@ def videoend():
             time2 = total_time.get_attribute('innerText')
             if time1 == time2:
                 video.click()
-                nextvideo = driver.find_element(By.CLASS_NAME, "nextButton") #定位下一个视频按钮
-                nextvideo.click()#切换到下一个视频
+                nextvideo = driver.find_element(By.CLASS_NAME, "nextButton")  # 定位下一个视频按钮
+                nextvideo.click()  # 切换到下一个视频
                 lesson()
+                print("运行成功")
         except:
             pass
         time.sleep(10)
@@ -81,5 +111,4 @@ t2 = threading.Thread(target=videoend)
 t2.start()
 t1.start()
 
-
-# version：1.0
+# version：1.1
